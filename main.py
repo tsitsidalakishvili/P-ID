48df9b280a787d7b511e4fa352fa0230fe0b0b51
1Tsitsi1 <92105321+1Tsitsi1@users.noreply.github.com>
2024-03-09 12:14:48 +0400

    commit

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ st.set_page_config(layout="wide")
 base_dir = 'P-ID'  # Alternatively, you can use '.' for the current directory
 
 # Relative path to your YOLOv5 directory from the base directory
-yolov5_rel_path = 'yolov51'
+yolov5_rel_path = 'yolov5'
 
 # Full path to the YOLOv5 directory
 yolov5_dir = os.path.join(base_dir, yolov5_rel_path)
