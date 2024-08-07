1855c30d9fc8b940e5f43723f0c09fde2b5f5e7d
tsitsidalakishvili <114109718+tsitsidalakishvili@users.noreply.github.com>
2024-08-07 12:32:29 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 # setup.sh
 #!/bin/bash
 
-# Install setuptools
+# Install setuptools using apt
 apt-get update && apt-get install -y python3-setuptools
+
+# Install setuptools using pip
+pip install setuptools
 
 # Install Python packages
 pip install -r requirements.txt
