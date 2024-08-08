7a87506090bc8d7e34278d9f0ad65c959001dfc5
tsitsidalakishvili <114109718+tsitsidalakishvili@users.noreply.github.com>
2024-08-08 12:08:57 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Install system dependencies
-apt-get update && apt-get install -y python3-distutils libgl1-mesa-glx libgl1-mesa-dri
+apt-get update && apt-get install -y libgl1-mesa-glx libgl1-mesa-dri
 
-# Ensure setuptools and pip are upgraded and installed correctly
+# Install Python distutils if not already present
+apt-get install -y python3-setuptools
+
+# Upgrade setuptools and pip to make sure we have the latest versions
 pip install --upgrade setuptools pip
 
 # Create the Streamlit configuration directory
