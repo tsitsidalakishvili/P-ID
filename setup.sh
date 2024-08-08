7a3dfb31eb51ef7e0d64831420ad872363c952a1
tsitsidalakishvili <114109718+tsitsidalakishvili@users.noreply.github.com>
2024-08-08 12:46:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 # Install system dependencies
 apt-get update && apt-get install -y libgl1-mesa-glx libgl1-mesa-dri python3-distutils python3-setuptools cmake
 
+# Explicitly install distutils and setuptools
+apt-get install -y python3-pip
+apt-get install -y python3-distutils
+
 # Upgrade setuptools and pip to make sure we have the latest versions
 pip install --upgrade setuptools pip
 
